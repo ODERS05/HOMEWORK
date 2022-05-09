f5b7e1581cb152ad8c45c27c2d9cb4f92a530634
Koshoi <koshoy.ibraev@gmail.com>
2022-05-09 15:15:13 +0600

    исправил cfg

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,26 @@
+import dao.NewsDao;
+import dao.NewscategoryDao;
+import dao.impl.NewsCategoryDaoImpl;
+import dao.impl.NewsDaoImpl;
+import entity.NewsCategoryEntity;
+import entity.NewsEntity;
+import enums.Category;
+
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
-
+        NewsDao newsDao = new NewsDaoImpl();
+        NewscategoryDao newscategoryDao = new NewsCategoryDaoImpl();
+        NewsCategoryEntity music = NewsCategoryEntity.builder()
+                .name(Category.MUSIC)
+                .build();
+        newscategoryDao.create(music);
+        NewsEntity news = NewsEntity.builder()
+                .postTime(LocalDateTime.now())
+                .newsText("some text")
+                .newsHeader("some text")
+                .newsCategory(music).build();
+        newsDao.create(news);
     }
 }
